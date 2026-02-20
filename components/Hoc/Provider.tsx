52737b2438f1c394fef995f1da8e952deb263e2d
AlaaSnayel <alaanayel111@gmail.com>
2026-02-20 10:27:14 +0200

    Dark & Light Mode

---
+++ components/Hoc/Provider.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "next-themes";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider attribute={"class"} enableSystem defaultTheme="system">
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Provider;
